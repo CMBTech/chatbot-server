3c2f19292d4b630d47de7ff4871d3023f27c75b5
MutegekiHenry <henrymutegeki117@gmail.com>
2021-08-21 18:22:42 +0300

    feat: add crud for categories

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,12 @@
 from flask_restful import Api
 from app.controllers import (
-    IndexView
+    IndexView, CategoryView
     )
 
 api = Api()
 
 # Index route
 api.add_resource(IndexView, '/')
+
+# Category routes
+api.add_resource(CategoryView, '/categories', endpoint='categories')
